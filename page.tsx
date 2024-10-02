5650ee30e02c2adc8b64ac78de046d0df2c8b290
Rimsha Ansari <159952102+leadership286@users.noreply.github.com>
2024-10-02 05:00:04 +0500

    Add files via upload

---
+++ page.tsx
@@ -0,0 +1,29 @@
+import Navbar from "../components/navbar/page";
+
+
+export default function jobs() {
+    return (
+    
+  <div className="jobs">
+    <Navbar/>
+    
+  <h1>Join Our Team at Educational World!</h1>
+  <p>We're looking for passionate individuals to help us.</p>
+  <p>Explore exciting career opportunities with us!</p>
+
+  <h2>Current Openings:</h2>
+  <ul>
+    <li>Web Developer</li>
+    <li>Content Creator</li>
+    <li>Data Analyst</li>
+    <li>Marketing Specialist</li>
+    <li>Customer Support</li>
+  </ul>
+
+
+    
+  </div>
+    )
+  }
+
+
